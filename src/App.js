79c833ab04fe309c37a26ab2d149e89b3df9b1de
Gabriel Cerutti <gabriel.cerutti@haufe-akademie.de>
2020-02-20 18:01:50 +0100

    Added redux demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React and Amplify sample application -  This is Dev!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           React and Amplify sample application -  This is Dev!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React from "react";
+import List from "./components/List";
+import Form from "./components/Form";
+import Post from "./components/Post";
+
+const App = () => (
+  <>
+    <div>
+      <h2>Articles</h2>
+        <List />
     </div>
-  );
-}
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
+    <div>
+      <h2>API posts</h2>
+      <Post />
+    </div>
+  </>
+);
 
 export default App;
